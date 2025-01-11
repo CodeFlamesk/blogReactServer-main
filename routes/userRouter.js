11be4816d1042d9b2bf0ba0ccfd31ba96b09eb9b
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2025-01-11 01:15:43 +0100

    done server

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,33 +1,33 @@
 const Router = require("express");
 const router = new Router();
 const UserController = require("../controllers/userController")
-const {body} = require("express-validator")
-const authMiddleware = require("../middleware/auth.middleware.js")
+const { body } = require("express-validator")
+
 
 
 router.post("/registration",
     body("email").isEmail(),
-    body("password").isLength({min:3, max:32}), 
+    body("password").isLength({ min: 3, max: 32 }),
     UserController.registration);
-router.post("/login", 
+router.post("/login",
     body("email").isEmail(),
     UserController.login);
-    
+
 router.post("/logout", UserController.logout);
 
 router.delete("/del/:id", UserController.deleteUser);
 
 router.get("/activate/:link", UserController.activate);
 router.get("/refresh", UserController.refresh);
-router.get("/users",authMiddleware,  UserController.getUsers);
+router.get("/users", UserController.getUsers);
 
 router.post("/forgot", UserController.forgotPassword);
-router.post("/password", 
+router.post("/password",
     body("email").isEmail(),
-    body("newPassword").isLength({min:3, max:32}),
+    body("newPassword").isLength({ min: 3, max: 32 }),
     UserController.changePasswordForgot);
-    
-router.post("/password/change",  UserController.changePassword);
+
+router.post("/password/change", UserController.changePassword);
 
 router.post("/avatar", UserController.addImage);
 router.delete("/avatar/:id", UserController.deleteAvatar);
