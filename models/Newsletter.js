b94642b38cd94c54db27942a03c69afaa53f480f
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2025-03-14 17:42:38 +0100

    ready

--- models/Newsletter.js
+++
@@ -1,7 +0,0 @@
-const {Schema, model} = require("mongoose")
-
-const Newsletter = new Schema({
-    email: {type: String, required:true, unique:true},
-})
-
-module.exports = model("Newsletter", Newsletter);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = new Router();
 const categoryRouter = require("./categoryRouter");
 const blogRouter = require("./blogRouter");
 const userRouter = require("./userRouter");
-const newsletterRouter = require("./newsletterRouter");
+
 const commentRouter = require("./commentRouter");
 const feedbackRouter = require("./feedbackRouter");
 const askRouter = require("./askRouter");
@@ -15,7 +15,6 @@ const teamRouter = require("./teamRouter");
 router.use("/category", categoryRouter);
 router.use("/blog", blogRouter);
 router.use("/user", userRouter);
-router.use("/newsletter", newsletterRouter);
 router.use("/comments", commentRouter);
 router.use("/feedback", feedbackRouter);
 router.use("/ask", askRouter);
